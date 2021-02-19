0842bd1ce37ca82ff8871ca8ff1df7be034bfc3e
Anders Latif <andl@kea.dk>
2021-02-19 10:20:29 +0100

    Implemented delta time

--- 2._Game_Loop/index.js
+++ 2._Game_Loop/index.js
@@ -1,11 +1,51 @@
 const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
+
+let circleX;
+let circleY;
+
+let lastTime;
+let requiredElapsed = 1000 / 66.666; // 15 fps
+
 
 function setup() {
-    context.fillStyle = "lightblue";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    circleX = canvas.width / 1000;
+    circleY = canvas.height / 1000;
+    draw();
 }
 
+function draw(now) {
+    requestAnimationFrame(draw);
+
+    if (!lastTime) {
+        lastTime = now;
+    }
+    const elapsedTime = now - lastTime;
+    if (elapsedTime > requiredElapsed) {
+        // do stuff
+
+        ctx.fillStyle = "lightblue";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+        circleX += 1;
+        circleY += 1;
+        
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(circleX, circleY, 50, 0, Math.PI*2);
+        ctx.fill();
+
+        // then the last step
+        lastTime = now;
+    }
+}
+
+
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
 
 window.addEventListener("load", () => {
     canvas.width = window.innerWidth;
