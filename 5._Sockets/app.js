50231dd20684f2ddd2cfca066becd12ff45a5ed0
Anders Latif <andl@kea.dk>
2021-03-05 09:13:27 +0100

    Pushed a working version of sockets that serves a canvas html file

--- 5._Sockets/app.js
+++ 5._Sockets/app.js
@@ -5,4 +5,8 @@ const io = require('socket.io')(server);
 
 io.on('connection', () => { /* … */ });
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/canvas.html");
+});
+
 server.listen(3000);
