48e0742577bb16f1807d030470ea27ee1ae1ea5c
Anders Latif <andl@kea.dk>
2021-03-05 11:49:00 +0100

    Initialized with the existing project code

---
+++ 2._Game_Loop/models/EmptyBox.js
@@ -0,0 +1,17 @@
+class EmptyBox extends Rectangle { 
+    constructor(x, y, width, height) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    draw(ctx, x, y, color) {
+        if (color) {
+            ctx.strokeStyle = color;
+        }
+        ctx.beginPath();
+        ctx.rect(x || this.x, y || this.y, this.width, this.height);
+        ctx.stroke();
+    }
+}
