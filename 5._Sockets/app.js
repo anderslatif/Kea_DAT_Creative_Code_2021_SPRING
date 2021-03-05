89fcd20ce959e53b5c8c7f34645545a070645d6f
Anders Latif <andl@kea.dk>
2021-03-05 09:25:45 +0100

    Setting up socket in the client

--- 5._Sockets/app.js
+++ 5._Sockets/app.js
@@ -3,8 +3,11 @@ const server = require('http').createServer(app);
 
 const io = require('socket.io')(server);
 
-io.on('connection', () => { /* … */ });
+io.on('connection', (socket) => { 
+    console.log("A socket joined");
+});
 
+// serve the html page through the server
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/canvas.html");
 });
